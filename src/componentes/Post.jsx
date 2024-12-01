993266805a0a87a0714abcf35efa55aaa4742b19
JulioCesar <amorim.jc@gmail.com>
2024-12-01 19:08:29 -0300

    formulário de comentário

--- src/componentes/Post.jsx
+++ src/componentes/Post.jsx
@@ -15,17 +15,28 @@ export function Post() {
         <time title='01 de dezembro de 2024' datetime="2024-12-01 16:14:39" className={styles.datetime}>Publicado há 1h</time>
       </header>
 
-      <div className={styles.content}>   
-          <p>Fala galeraa 👋</p>
-          <p>Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-          <p>👉 <a href="#">jane.design/doctorcare</a></p>
-          <p>
-            <a href="#">#novoprojeto </a> 
-            <a href="#">#nlw </a> 
-            <a href="#">#rocketseat</a>
-          </p>
-        
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉 <a href="#">jane.design/doctorcare</a></p>
+        <p>
+          <a href="#">#novoprojeto </a>
+          <a href="#">#nlw </a>
+          <a href="#">#rocketseat</a>
+        </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder='Escreva um comentário...'
+        />
+
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
