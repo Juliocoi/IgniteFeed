fc236867f50c79615e1e9f03a832063bc4f158e0
JulioCesar <amorim.jc@gmail.com>
2024-12-14 14:28:52 -0300

    Cria componente Avatar

--- src/componentes/Post.jsx
+++ src/componentes/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
@@ -6,7 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header className={styles.postHeader}>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/juliocoi.png" />
+          <Avatar src='http://www.github.com/juliocoi.png' />
           <div className={styles.authorInfo}>
             <strong>Júlio César</strong>
             <span>Web developer</span>
--- src/componentes/Sidebar.jsx
+++ src/componentes/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
   return (
@@ -9,10 +10,7 @@ export function Sidebar() {
         src='https://plus.unsplash.com/premium_photo-1685086785636-2a1a0e5b591f?q=50&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
       />
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src='http://www.github.com/juliocoi.png' alt=''
-        />
+        <Avatar src="http://www.github.com/juliocoi.png"/>
         <strong>Júlio César</strong>
         <span>Web Develop</span>
       </div>
